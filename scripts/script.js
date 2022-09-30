43f40128551e8fa6ed038c77c22d21da909b1eb1
Romer <romer@nostal.dev>
2022-09-30 12:58:23 +0200

    Add marker

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,19 @@ L.tileLayer(
     attribution: "© Stadia Maps",
   }
 ).addTo(map);
+
+var LeafIcon = L.Icon.extend({
+  options: {
+    iconUrl: "../assets/marker.png",
+    shadowUrl: "",
+    iconSize: [32.31, 39.65],
+    shadowSize: [],
+    iconAnchor: [16, 39],
+    shadowAnchor: [],
+    popupAnchor: [-3, -76],
+  },
+});
+
+L.marker([52.3376, 4.902], {
+  icon: new LeafIcon(),
+}).addTo(map);
